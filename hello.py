580f10429cd1e0a09174bc5956145d6d44547287
Jordan Hewitt <jordannh@sent.com>
2012-11-13 16:30:44 -0800

    Added a deny menthod to deny a name.

--- hello.py
+++ hello.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
+def deny(name):
+	if name=="jordan":
+		print "You do not have access!"
+	else:
+		print "You have access."
+
 def main():
 	name = raw_input("Enter your name: ")
 	print ("Hello, " + name)
+	deny(name)
 
 if __name__=="__main__":
 	main()
